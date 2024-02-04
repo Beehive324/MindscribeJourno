2671bb3246caeea30f862405ee128cad93a15172
Fairson Soares <Fairson123@hotmail.com>
2024-02-04 11:47:25 +0000

    landing page

--- ARTICUL-E/App.js
+++ ARTICUL-E/App.js
@@ -1,12 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer} from '@react-navigation/native'; 
+import {createNativeStackNavigator} from '@react-navigation/native-stack'
 
 export default function App() {
   return (
+    <NavigationContainer>
+      <Stack.Navigator>
     <View style={styles.container}>
       <Text>Articul-e</Text>
       <StatusBar style="auto" />
     </View>
+    </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
